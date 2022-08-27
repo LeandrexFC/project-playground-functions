11c59aa3cdcea9ff64c1b61e92401f32f1cdfe91
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-27 18:09:26 -0300

    Desafio 7 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -48,11 +48,15 @@ function highestCount(num) {
     return resultado;
 }
 
-
-
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(Math.abs (cat2 - mouse) < Math.abs(cat1 - mouse)) {
+    return "cat2"
+  }else if (Math.abs (cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return "cat1"
+  }else
+    return 'os gatos trombam e o rato foge'
+
 }
 
 // Desafio 8
